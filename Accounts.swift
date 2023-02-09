b51b391ed0da5fdcdbfbb3b608dbc2f15f5f854d
Kartik Paliwal <88022271+kartikpaliwal30@users.noreply.github.com>
2023-02-09 16:15:22 +0530

    Add files via upload

---
+++ Accounts.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class Acounts: UITableViewCell {
+    
+
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var Aname:UILabel!
+    @IBOutlet weak var Ano:UILabel!
+    @IBOutlet weak var Balance:UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
